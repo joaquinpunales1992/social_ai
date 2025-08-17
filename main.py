ef9c3a7ed242a9991688ab28a796a3db7699c9a9
joaquinp1992 <joaquinpunales@gmail.com>
2025-08-17 15:00:29 -0500

    update social ai name

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from social.facebook.routes import router as facebook_router
 from social.instagram.routes import router as instagram_rotuer
 
-app = FastAPI()
+app = FastAPI(
+    title="SocialAI API",
+    version="1.0.0",
+    contact={
+        "name": "API Support",
+        "email": "hello@akiyainjapan.com",
+    }
+    )
 
 app.include_router(facebook_router)
 app.include_router(instagram_rotuer)
